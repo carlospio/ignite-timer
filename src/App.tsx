072c86cf8601c37361a56b757941a57c979c6fcf
Carlos Pio <carlospiobenicio@gmail.com>
2023-10-04 19:30:17 -0300

    Commit: Páginas e rotas - React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import { defaultTheme } from './styles/themes/default'
 import {ThemeProvider} from 'styled-components'
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/global'
+import {BrowserRouter} from 'react-router-dom'
+import { Router } from './Router'
 
 export function  App() {
   return (
    
       <ThemeProvider theme={defaultTheme}>
-        <Button variant="danger"/>
-        <Button variant="primary" />
-        <Button variant="secundary" />
-        <Button variant="sucess" />
-        <Button  />
+        <BrowserRouter>
 
+          <Router />  
+          <GlobalStyle />
 
-
-        <GlobalStyle />
+        </BrowserRouter>       
       </ThemeProvider>
     
   )
