eb0c4644b895f46a1674122cd821d45d25ed0d77
Carlos Pio <carlospiobenicio@gmail.com>
2023-10-04 12:44:15 -0300

    Commit: Estrutura da aplicação - Cores & fonte

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -19,7 +19,10 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100%;
     height: 40px;
 
-    background-color: ${props => props.theme.primary}
+    background-color: ${props => props.theme['green-500']};
+
+    color: ${props=>props.theme['white']};
+
 
     /* ${props => {
         return `background-color: ${buttonVariant[props.variant]}`
