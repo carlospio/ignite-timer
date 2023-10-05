471078eb241e355a24bf6a4839febb41a923c440
Carlos Pio <carlospiobenicio@gmail.com>
2023-10-04 22:59:46 -0300

    Commit: Páginas e rotas - Componente: Status

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -52,7 +52,7 @@ export function Home() {
 
                 <StartCountdownButton
                     type="submit"
-                    
+                    disabled
 
                 >
                     <Play size={24} />
